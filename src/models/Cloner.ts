24ddb75f18d78dd785e1be0a5e0dd49eb00c94a1
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-11-30 10:33:55 +0530

    clone Source fixed

--- src/models/Cloner.ts
+++ src/models/Cloner.ts
@@ -18,6 +18,7 @@ export type PropertyPath = Array<(item) => any>;
 export class Cloner<T> {
 
     protected static copyProperties(src, dest = {}) {
+        dest[cloneSource] = src;
         for (const key in src) {
             if (Object.prototype.hasOwnProperty.call(src, key)) {
                 const element = src[key];
@@ -59,28 +60,27 @@ export class Cloner<T> {
         this.$type = (item as any).$type;
     }
 
-    public include<TProperty>(property: (item: T) => ICollection<TProperty>): PropertyCloner<T, TProperty>;
-    public include<TProperty>(property: (item: T) => TProperty[]): PropertyCloner<T, TProperty>;
-    public include<TProperty>(property: (item: T) => TProperty): PropertyCloner<T, TProperty>;
+    public include<TProperty>(property: (item: T) => ICollection<TProperty> | TProperty): PropertyCloner<T, TProperty>;
+    // public include<TProperty>(property: (item: T) => TProperty[]): PropertyCloner<T, TProperty>;
+    // public include<TProperty>(property: (item: T) => TProperty): PropertyCloner<T, TProperty>;
     public include<TProperty>(property: any): PropertyCloner<T, TProperty> {
         const last = [property];
         return new PropertyCloner(this.item, this.path ? [... this.path, last]: [last], last);
     }
 
     private clone(src, dest, paths) {
-        dest[cloneSource] = src;
         if (!paths.length) {
             return dest;
         }
-        let [first, ... others] = paths;
+        const [first, ... others] = paths;
         let p = first(src);
         if (!p) {
             return dest;
         }
-        let original = p;
-        let name = getKey(src, p);
+        const original = p;
+        const name = getKey(src, p);
         if (Array.isArray(p)) {
-            let existing = dest[name] ??= [];
+            const existing = dest[name] ??= [];
             for (let index = 0; index < p.length; index++) {
                 const element = p[index];
                 const existingItem = existing[index] ??= {};
@@ -101,13 +101,11 @@ export class Cloner<T> {
 export class PropertyCloner<T, TPrevious> extends Cloner<T> {
 
     constructor(item: T, path: PropertyPath[], private propertyPath: PropertyPath) {
-        super(item ,path);
+        super(item, path);
     }
-    
 
-    public thenInclude<TProperty>(property: (item: TPrevious) => ICollection<TProperty>): PropertyCloner<T, TProperty>;
-    public thenInclude<TProperty>(property: (item: TPrevious) => TProperty[]): PropertyCloner<T, TProperty>;
-    public thenInclude<TProperty>(property: (item: TPrevious) => TProperty): PropertyCloner<T, TProperty>;
+    public thenInclude<TProperty>(
+        property: (item: TPrevious) => ICollection<TProperty> | TProperty): PropertyCloner<T, TProperty>;
     public thenInclude<TProperty>(property: any): PropertyCloner<T, TProperty> {
         this.propertyPath.push(property);
         return new PropertyCloner(this.item, this.path, this.propertyPath);
