d8b010b328dedd4631e457ceb5e109d5e7b38c8a
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-11-30 13:04:24 +0530

    merge by path added

--- src/services/mergeProperties.ts
+++ src/services/mergeProperties.ts
@@ -1,16 +1,75 @@
 import cloneSource from "../models/cloneSource";
 
-export default function mergeProperties(src, target, visited = new Map()) {
-    if (visited.has(src)) {
+// export default function mergeProperties(src, target, visited = new Map()) {
+//     if (visited.has(src)) {
+//         return;
+//     }
+//     visited.set(src, true);
+//     if (Array.isArray(src)) {
+//         for (let index = 0; index < src.length; index++) {
+//             const srcElement = src[index];
+//             const targetElement = target[index];
+//             if (targetElement) {
+//                 mergeProperties(srcElement, targetElement, visited);
+//                 continue;
+//             }
+//             target[index] = targetElement;
+//         }
+//         return;
+//     }
+
+//     const cloneTarget = target[cloneSource];
+
+//     for (const key in src) {
+//         if (Object.prototype.hasOwnProperty.call(src, key)) {
+//             const srcElement = src[key];
+//             if (typeof srcElement !== "object") {
+//                 if (srcElement !== void 0) {
+//                     target[key] = srcElement;
+//                     if (cloneTarget) {
+//                         cloneTarget[key] = srcElement;
+//                     }
+//                 }
+//                 continue;
+//             }
+//             // check if it is a date...
+//             if (srcElement instanceof Date) {
+//                 target[key] = srcElement;
+//                 if (cloneTarget) {
+//                     cloneTarget[key] = srcElement;
+//                 }
+//                 continue;
+//             }
+//             const targetElement = target[key];
+//             if (targetElement) {
+//                 if (cloneTarget) {
+//                     const cloneTargetElement = cloneTarget[key];
+//                     if (cloneTargetElement) {
+//                         mergeProperties(srcElement, cloneTargetElement, visited);
+//                     }
+//                 }
+//                 mergeProperties(srcElement, targetElement, visited);
+//                 continue;
+//             }
+//             target[key] = targetElement;
+//             if (cloneTarget) {
+//                 cloneTarget[key] = targetElement;
+//             }
+//         }
+//     }
+// }
+
+export default function mergeProperties(src, target, path: string = "0", visited = new Map()) {
+    if (visited.has(path)) {
         return;
     }
-    visited.set(src, true);
+    visited.set(path, true);
     if (Array.isArray(src)) {
         for (let index = 0; index < src.length; index++) {
             const srcElement = src[index];
             const targetElement = target[index];
             if (targetElement) {
-                mergeProperties(srcElement, targetElement, visited);
+                mergeProperties(srcElement, targetElement, `${path}.${index}`, visited);
                 continue;
             }
             target[index] = targetElement;
@@ -45,10 +104,10 @@ export default function mergeProperties(src, target, visited = new Map()) {
                 if (cloneTarget) {
                     const cloneTargetElement = cloneTarget[key];
                     if (cloneTargetElement) {
-                        mergeProperties(srcElement, cloneTargetElement, visited);
+                        mergeProperties(srcElement, cloneTargetElement, path + ".$" + key, visited);
                     }
                 }
-                mergeProperties(srcElement, targetElement, visited);
+                mergeProperties(srcElement, targetElement, path + "." + key, visited);
                 continue;
             }
             target[key] = targetElement;
